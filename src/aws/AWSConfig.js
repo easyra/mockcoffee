8cbc088bd80b0b49f8e0f3f160d98bb422118a01
ezra davis davis <ezra.rr.davis@gmail.com>
2019-05-15 15:13:48 -0700

    140 lb twink get pounded by chad env variable

--- src/aws/AWSConfig.js
+++ src/aws/AWSConfig.js
@@ -4,7 +4,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({
   region: 'us-west-2',
   endpoint: 'https://dynamodb.us-west-2.amazonaws.com',
-  accessKeyId: process.env.REACT_APP_AWS_KEY,
+  accessKeyId: process.env.REACT_APP_AWS_ID,
   secretAccessKey: process.env.REACT_APP_AWS_KEY
 });
 
