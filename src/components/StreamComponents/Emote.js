01b6871e8a56b61f55da1456686ecfceebaaaf10
ezra davis davis <ezra.rr.davis@gmail.com>
2019-05-25 13:53:49 -0700

    change emote css

--- src/components/StreamComponents/Emote.js
+++ src/components/StreamComponents/Emote.js
@@ -4,8 +4,8 @@ const Emote = ({ emoteName, src }) => {
   return (
     <div className={`emote ${emoteName}`}>
       {' '}
-      <img src={src} alt={emoteName} />{' '}
+      <img src={src} alt={emoteName} title={emoteName} />{' '}
     </div>
   );
 };
-export default Emote
+export default Emote;
